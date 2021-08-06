853b022cc802e9f6f11158221187fa9d73404674
Shunji Zhan <shunhahaha@live.com>
2021-08-06 18:05:35 +0800

    able to show buttons and icos

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,58 @@ import {
   InputGroup,
   Input,
   Image,
+  Stack,
+  Box,
+  Flex,
+  Text,
 } from "@chakra-ui/react"
 import {
   MdSettingsVoice,
   MdSearch,
+  MdAdd,
 } from 'react-icons/md';
 import styles from '../styles/Home.module.css'
 
+const sanitizeUrl = url => {
+  return url.startsWith('https://')
+    ? url
+    : 'https://' + url;
+}
+
+const dummyFn = () => {};
+const Tile = ({ url, name, icon, onClick = dummyFn }) => (
+  <Flex
+    direction='column'
+    justifyContent='center'
+    alignItems='center'
+    as="button"
+    height="100px"
+    width="100px"
+    border="none"
+    borderRadius="5px"
+    fontSize="14px"
+    _hover={{ bg: "#eeeeee" }}
+    onClick={ onClick }
+  >
+    { url &&
+      <Image
+        height="10"
+        width="10"
+        src={sanitizeUrl(`${url}/favicon.ico`)}
+        alt='tileImage'
+      />
+    }
+
+    { icon }
+    
+    <Text p='10px 0 0 0 '>{ name }</Text>
+  </Flex>
+);
+
 export default function Home() {
+  const openModal = () => {
+    console.log('!!!');
+  }
   return (
     <div className={ styles.container }>
       <Head>
@@ -56,7 +100,24 @@ export default function Home() {
           />
         </InputGroup>
 
-        hello
+        <Stack
+          w='550px'
+          direction='row'
+          spacing="0"
+          flexWrap='wrap'
+          paddingTop="20px"
+        >
+          <Tile url="github.com" name='github' />
+          <Tile url="google.com" name='google'/>
+          <Tile url="baidu.com" name='baidu'/>
+          <Tile url="leetcode-cn.com" name='leetcode'/>
+          <Tile url="https://edu.lagou.com" name='拉钩'/>
+          <Tile url="developer.mozilla.org" name='MDN'/>
+          <Tile url="https://stackoverflow.com" name='stackoverflow'/>
+          <Tile url="taobao.com" name='taobao'/>
+          <Tile url="qq.com" name='QQ'/>
+          <Tile icon={ <MdAdd style={{ width: 40, height: 40}} />} name='add new' onClick={ openModal }/>
+        </Stack>
       </main>
 
     </div>
